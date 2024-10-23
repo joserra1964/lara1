6680096d6f032815a82f3be8e32dd2c77ba86adc
Joserra <j.parada.vigo@gmail.com>
2024-10-23 13:30:40 +0200

    dia 23 los models funcionan

--- routes/web.php
+++ routes/web.php
@@ -6,20 +6,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
-
-Route::get('blog', function() {
-// Consulta a BBDD
-$posts = [
-    ['id'=>1, 'title'=>'PHP', 'slug'=>'php'],
-    ['id'=>2, 'title'=>'Laravel', 'slug'=>'laravel']
-];
-return view('blog',['posts'=>$posts]);
-});
-
-Route::get('blog/{slug}',function($slug) {
-    // Consulta a BBDD
-    $post = $slug;
-    return view('post',['post'=>$post]);
-});
-
-
